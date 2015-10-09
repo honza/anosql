b6e5d2413dfcb73f8e288e691488c0da24e1e213
Honza Pokorny <me@honza.ca>
2015-10-09 09:42:34 -0300

    0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setup(
     name='anosql',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/honza/anosql',
     install_requires=[],
     description='Easy SQL in Python',
